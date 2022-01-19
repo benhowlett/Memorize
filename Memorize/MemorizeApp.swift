1155ef83dbc32208e9e6852e0c6e1da26dfb370c
Ben Howlett <ben@benhowlett.com>
2022-01-19 16:33:33 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ben Howlett on 2022-01-19.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
