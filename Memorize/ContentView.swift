d1b5c74f23a0d444fbcbe361fb052a50e85c2c6e
Ben Howlett <ben@benhowlett.com>
2022-01-23 07:56:08 -0700

    Removed the buttons on the bottom for adding and removing cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -26,9 +26,7 @@ struct ContentView: View {
             .foregroundColor(.red)
             Spacer()
             HStack {
-                remove
-                Spacer()
-                add
+                
             }
             .padding(.horizontal)
             .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
@@ -36,25 +34,25 @@ struct ContentView: View {
         .padding(.horizontal)
     }
     
-    var remove: some View {
-        Button {
-            if emojiCount > 1 {
-                emojiCount -= 1
-            }
-        } label: {
-            Image(systemName: "minus.circle")
-        }
-    }
-    
-    var add: some View {
-        Button {
-            if emojiCount < emojis.count {
-                emojiCount += 1
-            }
-        } label: {
-            Image(systemName: "plus.circle")
-        }
-    }
+//    var remove: some View {
+//        Button {
+//            if emojiCount > 1 {
+//                emojiCount -= 1
+//            }
+//        } label: {
+//            Image(systemName: "minus.circle")
+//        }
+//    }
+//
+//    var add: some View {
+//        Button {
+//            if emojiCount < emojis.count {
+//                emojiCount += 1
+//            }
+//        } label: {
+//            Image(systemName: "plus.circle")
+//        }
+//    }
 }
 
 struct CardView: View {
