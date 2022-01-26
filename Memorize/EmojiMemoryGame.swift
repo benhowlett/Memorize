d99157eb574177c5ff3d09b6c3d8031d1474eaef
Ben Howlett <ben@benhowlett.com>
2022-01-26 13:43:31 -0700

    Finished setting up MVVM structure Added logic to flip the cards to the Model. Added the ability for the View to tell the ViewModel the user's intent to choose a card. Added the ability for the ViewModel to handle the user's intent.

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
+class EmojiMemoryGame: ObservableObject {
     static let emojis = ["🚗", "🛵", "🚀", "🚂", "🛻", "🚚", "🚜", "🛺", "🚔", "✈️", "🚁", "🛸", "🛶", "⛵️", "🚛", "🏍", "🚖", "🚒", "🚐", "🛴", "🚲", "🚃", "🚈", "🚤"]
     
     static func createMemoryGame() -> MemoryGame<String> {
@@ -15,10 +15,16 @@ class EmojiMemoryGame {
                 emojis[pairIndex]
         }
     }
-    
-    private var model: MemoryGame<String> = createMemoryGame()
+        
+    @Published private var model: MemoryGame<String> = createMemoryGame()
     
     var cards: Array<MemoryGame<String>.Card> {
         model.cards
     }
+    
+    // MARK: - Intent(s)
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
+    
 }
