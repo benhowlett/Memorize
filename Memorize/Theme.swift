82da528717bc15f11ace5f5f26ca06c63225d24a
Ben Howlett <ben@benhowlett.com>
2022-01-29 11:45:57 -0700

    Added a new swift file for the Theme struct in the data model

---
+++ Memorize/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  Memorize
+//
+//  Created by Ben Howlett on 2022-01-29.
+//
+
+import SwiftUI
+
+struct Theme {
+    var name: String
+    var emojis: [String]
+    var numberOfPairsOfCards: Int
+    var color: Color
+}
