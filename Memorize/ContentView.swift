fb35ffe99b5104a774013d7fe0027253381e7ca1
Ben Howlett <ben@benhowlett.com>
2022-01-28 18:25:57 -0700

    Introduced Optionals to the Model. Handled the case where a card that isn't from the right game is passed to the Model

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
             Text("Memorize!")
                 .font(.largeTitle)
             ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
                     ForEach(viewModel.cards) {card in
                         CardView(card: card)
                             .aspectRatio(2/3, contentMode: .fit)
--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -11,23 +11,23 @@ struct MemoryGame<CardContent> {
     private(set) var cards: Array<Card>
     
     mutating func choose(_ card: Card) {
-        let chosenIndex = index(of: card)
-        cards[chosenIndex].isFaceUp.toggle()
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id}) {
+            cards[chosenIndex].isFaceUp.toggle()
+        }
         print("\(cards)")
     }
     
-    func index(of card: Card) -> Int {
+    func index(of card: Card) -> Int? {
         for index in 0..<cards.count {
             if cards[index].id == card.id  {
                 return index
             }
         }
-        return 0
+        return nil
     }
     
     init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
         cards = Array<Card>()
-        // add numberOfPairsOfCards x 2 cards to the cards array
         for pairIndex in 0..<numberOfPairsOfCards {
             let content = createCardContent(pairIndex)
             cards.append(Card(content: content, id: pairIndex*2))
