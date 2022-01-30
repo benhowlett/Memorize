04aee75822c2336fc93dc8e97595aedc620ce800
Ben Howlett <ben@benhowlett.com>
2022-01-29 22:08:40 -0700

    Added multiple themes to the ViewModel. Added functionality to randomly select a theme when a MemoryGame is created

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 class EmojiMemoryGame: ObservableObject {
-    static let emojis = ["🚗", "🛵", "🚀", "🚂", "🛻", "🚚", "🚜", "🛺", "🚔", "✈️", "🚁", "🛸", "🛶", "⛵️", "🚛", "🏍", "🚖", "🚒", "🚐", "🛴", "🚲", "🚃", "🚈", "🚤"]
+    static var vehicleTheme = Theme(name: "Vehicles", emojis: ["🚗", "🛵", "🚀", "🚂", "🛻", "🚚", "🚜", "🛺", "🚔", "✈️", "🚁", "🛸", "🛶", "⛵️", "🚛", "🏍", "🚖", "🚒", "🚐", "🛴", "🚲", "🚃", "🚈", "🚤"], numberOfPairsOfCards: 8, color: .red)
+    static var flagTheme = Theme(name: "Flags", emojis: ["🇨🇦", "🏳️‍🌈", "🇦🇺", "🇬🇧", "🇺🇸", "🏴󠁧󠁢󠁳󠁣󠁴󠁿", "🇩🇰", "🇭🇺", "🇬🇷", "🇵🇫", "🇬🇾", "🇩🇪", "🇭🇰", "🇯🇵", "🇯🇲", "🇮🇱", "🇮🇩", "🇲🇸", "🇲🇰", "🇵🇬"], numberOfPairsOfCards: 10, color: .blue)
+    static var animalTheme = Theme(name: "Animals", emojis: ["🐒", "🐔", "🐧", "🦆", "🦅", "🦉", "🐺", "🦄", "🐝", "🦂", "🐳", "🦖", "🐫", "🐈", "🐉", "🦨", "🕊", "🦌", "🦥", "🐇"], numberOfPairsOfCards: 12, color: .green)
+    static var toolTheme = Theme(name: "Tools", emojis: ["🔨", "🪛", "🔧", "⛏", "🪚", "🪓", "🧲", "🧰", "🪜", "⚙️", "🧱", "🔪"], numberOfPairsOfCards: 10, color: .purple)
+    static var sportsTheme = Theme(name: "Sports", emojis: ["⚽️", "🏀", "🏈", "⚾️", "🎾", "🥏", "🏒", "🏸"], numberOfPairsOfCards: 8, color: .yellow)
+    static var musicTheme = Theme(name: "Music", emojis: ["🎤", "🎧", "🎼", "🎹", "🥁", "🎷", "🎺", "🪗", "🎸", "🪕", "🎻"], numberOfPairsOfCards: 10, color: .orange)
+    
+    static var themes: [Theme] = [vehicleTheme, flagTheme, animalTheme, toolTheme, sportsTheme, musicTheme]
     
     static func createMemoryGame() -> MemoryGame<String> {
-        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
-                emojis[pairIndex]
+        let randomThemeIndex = Int.random(in: 0..<6)
+        let shuffledEmojis = themes[randomThemeIndex].emojis.shuffled()
+        
+        if themes[randomThemeIndex].numberOfPairsOfCards > shuffledEmojis.count {
+            themes[randomThemeIndex].numberOfPairsOfCards = shuffledEmojis.count
+        }
+        
+        return MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+                shuffledEmojis[pairIndex]
         }
     }
         
